0518ad03ffb6d0094a8fb3fd051ba5b9ad044af0
Kurt Rose <doublereedkurt@gmail.com>
2012-07-09 00:36:26 -0700

    working on 2D texture rendering

--- game.py
+++ game.py
@@ -22,36 +22,6 @@ DIR = str(Filename.fromOsSpecific(os.path.dirname(os.path.abspath(__file__))))
 ASSET_DIR = DIR+"/assets/"
 PM.getModelPath().appendPath(ASSET_DIR) 
 
-def make_grid(name, rows, cols, size, thickness):
-    lines = LineSegs()
-    lines.setColor(PM.VBase4(0,0,0,1))
-    lines.setThickness(thickness)
-    #compute some constants
-    x_size = cols*size
-    y_size = rows*size
-    bottom = -y_size/2
-    top    =  y_size/2
-    left   = -x_size/2
-    right  =  x_size/2
-    #draw vertical lines
-    for i in range(0, cols):
-        x_pos = i*size - left
-        lines.moveTo(x_pos, bottom, 0)
-        lines.drawTo(x_pos, top, 0)
-    #draw horizontal lines
-    for i in range(0, rows):
-        y_pos = i*size - bottom
-        lines.moveTo(left,  y_pos, 0)
-        lines.drawTo(right, y_pos, 0)
-    #set up nodes and paths
-    node       = PandaNode(name)
-    nodepath   = NodePath(node)
-    lines_node = lines.create()
-    lines_node_path = NodePath(lines_node)
-    lines_node_path.setAntialias(PM.AntialiasAttrib.MLine)
-    lines_node_path.reparentTo(nodepath)
-    return nodepath
-
 class RockSlabFactory(object):
     def __init__(self, app):
         self.app = app
@@ -82,30 +52,36 @@ class RockSlabFactory(object):
         for i in range(num):
             cur_radius = radius*math.sin(math.acos( abs(1 - i*2.0/num) ))
             cur_slab = self.make_slab(cur_radius, cur_radius)
+            cur_slab.reparentTo(stack_node_path)
             #offset to center smaller items in sphere
             offset = (radius - cur_radius)/2.0
             cur_slab.setPos(offset, i*spacing, offset)
         stack_node_path.reparentTo(self.app.render)
         return stack_node_path
 
+class TextureRenderer(object):
+    def __init__(self):
+        self.buffer  = base.win.makeTextureBuffer("RoidSurface", 512, 512)
+        self.texture = self.buffer.getTexture()
+        self.buffer.setSort(-100) #ensure this buffer is rendered before main window
+        self.camera  = base.makeCamera2d(self.buffer)
+        self.scene   = NodePath("TextureScene")
+        self.camera.reparentTo(self.scene)
+
+
 class App(ShowBase.ShowBase):
     def __init__(self):
         ShowBase.ShowBase.__init__(self)
-        #self.grid = make_grid("grid", 10, 10, 20, 10)
-        #self.grid.setPos(0, 0, 0)
-        #self.grid.reparentTo(self.render)
+        self.texture_renderer = TextureRenderer()
         
         self.rock_fact = RockSlabFactory(self)
-        for i in range(2):
+        for i in range(3):
             a = self.rock_fact.make_spheroid_stack(100, 20, "name"+str(i))
-            a.setPos(0, 0, i*20)
-        
-        b = self.rock_fact.make_stack(20, 20, 4, 10)
-        b.setPos(100, 0, 0)
+            a.setPos(150*i, 0, 0)
         
         self.init_skybox()
         self.init_ui()
-        self.init_shaders()
+        #self.init_shaders()
         self.taskMgr.add(self.camera_task, "cameraTask")
     
     def camera_task(self, task):
@@ -139,5 +115,6 @@ class App(ShowBase.ShowBase):
             print "info: graphics card does not support shaders"
 
 app = App()
+PM.PStatClient.connect()
 app.run()
 
